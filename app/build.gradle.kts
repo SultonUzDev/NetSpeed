02d677f904545ab76ee6b37dadf9b64dc84e061f
Sulton21317 <sultontatu21317@gmail.com>
2026-09-22 11:50:42 +0500

    ci: add release workflow for Google Play internal track

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.sultonuzdev.netspeed"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
